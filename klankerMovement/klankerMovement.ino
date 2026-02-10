20930e9bf7fc162ca807fb1b5d72083f2395bd04
Jakub <jakub.mazur@student.nhlstenden.com>
2026-02-10 14:38:01 +0100

    added klaner movement

---
+++ klankerMovement/klankerMovement.ino
@@ -0,0 +1,19 @@
+const int LeftForward = 11;
+const int LeftBackward = 10;
+const int RightBackward = 3;
+const int RightForward = 5;
+const int RotatorOne = 9;
+const int RotatorTwo = 6;
+
+void setup() {
+  pinMode(LeftForward, OUTPUT);
+  pinMode(LeftBackward, OUTPUT);
+  pinMode(RightBackward, OUTPUT);
+  pinMode(RightForward, OUTPUT);
+  pinMode(RotatorOne, OUTPUT);
+  pinMode(RotatorTwo, OUTPUT);
+}
+
+void loop() {
+
+}
