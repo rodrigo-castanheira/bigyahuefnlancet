f35571b4becc3b216f77388ea32a1e19ad55c8a6
Jakub <jakub.mazur@student.nhlstenden.com>
2026-02-10 13:27:26 +0100

    improved exercises

--- jakub assignments/trafficLightExercise/trafficLightExercise.ino
+++ jakub assignments/trafficLightExercise/trafficLightExercise.ino
@@ -1,5 +1,5 @@
 const int LedRed = 8;
-const int LedGreen = 2;
+const int LedGreen = 13;
 const int LedYellow = 12;
 const int trafficButton = 4;
 
@@ -11,20 +11,21 @@ void setup() {
   pinMode(LedYellow, OUTPUT);
   pinMode(trafficButton, INPUT_PULLUP);
 
-  digitalWrite(LedRed, HIGH);
+  digitalWrite(LedGreen, HIGH);
+  digitalWrite(LedYellow, HIGH);
 }
 
 void loop() {
   if(digitalRead(trafficButton) == LOW){
     delay(50);
-    digitalWrite(LedRed, LOW);
-    delay(trafficDelay);
-    digitalWrite(LedGreen, HIGH);
-    delay(3 * trafficDelay);
-    digitalWrite(LedGreen, LOW);
-    digitalWrite(LedYellow, HIGH);
-    delay(trafficDelay);
-    digitalWrite(LedYellow, LOW);
     digitalWrite(LedRed, HIGH);
+    delay(trafficDelay);
+    digitalWrite(LedGreen, LOW);
+    delay(3 * trafficDelay);
+    digitalWrite(LedGreen, HIGH);
+    digitalWrite(LedYellow, LOW);
+    delay(trafficDelay);
+    digitalWrite(LedYellow, HIGH);
+    digitalWrite(LedRed, LOW);
     }
 }
